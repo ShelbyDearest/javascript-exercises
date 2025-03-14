d340e7d9bcf090d599309a8e5f754b3c00cb8e77
ShelbyDearest <shelbstabby@yahoo.com>
2025-03-13 20:49:20 -0500

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+  if (num < 0) {
+    return "ERROR"; }
+
+  let word = string.repeat;
+
+  for (i = 0; i < num; i++) {
+    word += string.repeat; }
+
+  return string.repeat(num);
 };
 
 // Do not edit below this line
