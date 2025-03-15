00cf3c72621e6d3854495b8421e7b37ce4c7849b
ShelbyDearest <shelbstabby@yahoo.com>
2025-03-14 20:16:57 -0500

    Update reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(string, revString) {
 
+  revString = string.split("").reverse().join("");
+  return revString
+  
 };
 
 // Do not edit below this line
