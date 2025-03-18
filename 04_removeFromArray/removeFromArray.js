45ffe514c18b54abe6e9e77120317591f6ca7afa
ShelbyDearest <shelbstabby@yahoo.com>
2025-03-18 00:24:21 -0500

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(firstArray, ...args) {
+
+    const newArray = [];
+
+    firstArray.forEach((items) => {
+        if (!args.includes(items)) {
+            newArray.push(items); } }); 
+        
+        return newArray; };
 
 // Do not edit below this line
 module.exports = removeFromArray;
