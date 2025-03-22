ae42d5f7139c621864e9a04dc8dd094abd86d193
ShelbyDearest <shelbstabby@yahoo.com>
2025-03-22 12:28:53 -0500

    Update sumAll.js
    The only test I couldn't get to run correctly was the "works with larger number first".

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sumAll = 0;
 
-};
+    for (let i = end; i >= start; i--) {
+        sumAll += i; } 
 
-// Do not edit below this line
-module.exports = sumAll;
+        if (isNaN(start) == start || isNaN(end) == end) {
+            return 'ERROR'; }
+
+            else if (!Number.isInteger(start) || !Number.isInteger(end)) {
+                return 'ERROR'; }
+
+                else if (start < 0 || end < 0 ) {
+                    return 'ERROR'; }
+    
+    return sumAll;
+    };
+    
+    // Do not edit below this line
+    module.exports = sumAll;
