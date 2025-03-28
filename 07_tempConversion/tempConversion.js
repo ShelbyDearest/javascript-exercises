e87b3164eda7abe736b790a918f5aa35f497db8a
ShelbyDearest <shelbstabby@yahoo.com>
2025-03-27 19:43:13 -0500

    Update tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,26 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(x) {
 
-const convertToFahrenheit = function() {
+  let Celsius = 0;
+  let Fahrenheit = x;
+
+  result = (x - 32) * 5/9;
+  
+  Celsius = Math.round(result * 10) / 10;
+
+  return Celsius;
+}
+;
+
+const convertToFahrenheit = function(x) {
+
+  let Fahrenheit = 0;
+  let Celsius = x;
+
+  result = (x * 9/5 + 32);
+
+  Fahrenheit = Math.round(result * 10) / 10;
+
+  return Fahrenheit;
 };
 
 // Do not edit below this line
